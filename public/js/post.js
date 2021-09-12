f78af3de708e277763242c42d1711eb822b06465
Asarvarjon <amriyevsarvarjon@yandex.com>
2021-09-12 17:59:06 +0500

    admin panel added

--- public/js/post.js
+++ public/js/post.js
@@ -7,9 +7,22 @@ const bookPhoto = document.querySelector("#file")
 const bookDesc = document.querySelector("#textarea");
 
 
-AdminForm.addEventListener("submit", (event) => {
+AdminForm.addEventListener("submit", async (event) => {
     event.preventDefault()
 
-    console.log(bookName.value, bookAuthor.value, bookPrice.value, bookPhoto.value, bookDesc.value);
+    const formData = new FormData();
+
+    formData.append("name", bookName.value);
+    formData.append("author", bookAuthor.value);
+    formData.append("price", bookPrice.value);
+    formData.append("content", bookDesc.value);
+    formData.append("photo", bookPhoto.files[0])
+
+    let response = await fetch("/admin", {
+        method: "POST",
+        body: formData,
+    });
+
+    console.log(response);
 })
 
